d1050adbbb55d78d4938a2e55892d31fa31c80f8
giomiglietta <maurizio.miglietta1@marist.edu>
2022-05-22 19:54:30 -0400

    Added Feature
    Will not stop when person leaves. Pauses until the person gets back

--- 2bots.py
+++ 2bots.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
+from discord.ext.commands import CommandInvokeError
 from discord import Guild, Member
 import os, discord, time, asyncio
 from dotenv import load_dotenv
+from discord.errors import HTTPException
 
 load_dotenv()
 
@@ -12,7 +14,6 @@ bot2 = discord.ext.commands.Bot(command_prefix="&")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_TOKEN2 = os.getenv("DISCORD_TOKEN2")
 
-num_of_channels = 0
 index = 0
 
 # Finds Voice Channels
@@ -22,52 +23,66 @@ async def channels(ctx):
     global num_of_channels
     voice_channel_list = ctx.guild.voice_channels
     num_of_channels = len(voice_channel_list)
-    return voice_channel_list
+    return voice_channel_list, num_of_channels
 
 
 @bot.command(name="move", pass_context=True)
 async def partyTime(ctx, member: discord.Member, seconds):
     global index
-    list = await channels(ctx)
+    index = 0
+    list, num_of_channels = await channels(ctx)
     t_end = time.time() + int(seconds)
     while time.time() < t_end:
         await asyncio.sleep(1)
         try:
             await member.move_to(list[index])
+            local = list[index]
         except IndexError:
+            list = await channels(ctx)
             await member.move_to(list[0])
+        except HTTPException:
+            while True:
+                try:
+                    await member.move_to(list[index])
+                    break
+                except HTTPException:
+                    await asyncio.sleep(1)
+                    t_end += 1
+        print("MOVED", local, index)
         if index == num_of_channels - 1:
             index = 0
         else:
             index += 1
-        try:
-            local = list[index]
-        except IndexError:
-            pass
-        print("MOVED", local, index)
 
 
 @bot2.command(name="move", pass_context=True)
 async def partyTime(ctx, member: discord.Member, seconds):
-    global index
-    list = await channels(ctx)
+    global index, num_of_channels
+    index = 0
+    list, num_of_channels = await channels(ctx)
     t_end = time.time() + int(seconds)
     await asyncio.sleep(1)
     while time.time() < t_end:
         await asyncio.sleep(1)
         try:
             await member.move_to(list[index])
+            local = list[index]
         except IndexError:
+            list, num_of_channels = await channels(ctx)
             await member.move_to(list[index - 1])
+        except HTTPException:
+            while True:
+                try:
+                    await member.move_to(list[index])
+                    break
+                except HTTPException:
+                    await asyncio.sleep(1)
+                    t_end += 1
+        print("MOVED 2", local, index)
         if index == num_of_channels - 1:
             index = 0
         else:
             index += 1
-        try:
-            local = list[index]
-        except IndexError:
-            pass
-        print("MOVED 2", local, index)
 
 
 loop = asyncio.get_event_loop()
